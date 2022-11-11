5322ad7070308e7905bfc5c1864f51f6a4594555
Jonathan Monteiro <88221018+Monteiro712@users.noreply.github.com>
2022-11-11 01:13:05 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function pegarNome(){
-    var Pname = document.getElementById('pInicial')
-    var pegar_Nome = document.getElementsByTagName('input')
-    Pname.textContent+=pegar_Nome[0].value
-}
